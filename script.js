043044696dc0d2eadb3d4a9cee9cc1ea18448ce0
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 20:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname = document.getElementById("f");
+	let lname = document.getElementById("l");
+	alert(fname.value , lname.value);
 }
